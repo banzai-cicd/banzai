e55d93ec6aad324bba34c1755f4d66811307d326
Simon Townsend <simon.townsend1@ge.com>
2017-04-10 14:04:27 -0500

    testing 1 2

--- vars/sast.groovy
+++ vars/sast.groovy
@@ -7,32 +7,38 @@ def call(body) {
 
     // now build, based on the configuration provided
     node {
+      stage ('SAST Tests') {
 
-      def CHECKMARX_TEAM = /CxServer\\SP\\GE\\GE_PowerWater\\mdi_12782/
-      def CHECKMARX_APP = "\\${config.appName}-${env.BRANCH_NAME}"
-      def PROJECT_NAME = "${CHECKMARX_TEAM}${CHECKMARX_APP}"
-      // checkout([$class: 'GitSCM', branches: [[name: "${branch}"]], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'MessageExclusion', excludedMessage: '.*\\[skip-ci\\].*'], [$class: 'LocalBranch', localBranch: "${branch}"]], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'ge-git', url: "${config.cloneAppUrl}"]]])
+        dir('scm') {
+          checkout scm
+        }
 
-      withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: 'ge-checkmarx',
-                                 usernameVariable: 'CHECKMARX_USER', passwordVariable: 'CHECKMARX_PASSWORD']]) {
-        sh """#!/bin/bash
-          echo Performing /opt/CxConsolePlugin/runCxConsole.sh scan -v    \
-          -ProjectName "${PROJECT_NAME}"    \
-          -CxServer 'https://checkmarx.security.ge.com'     \
-          -CxUser '$CHECKMARX_USER'   \
-          -CxPassword '$CHECKMARX_PASSWORD'   \
-          -preset 'Default 2014'    \
-          -locationtype folder      \
-          -locationpath '$WORKSPACE'
-          /opt/CxConsolePlugin/runCxConsole.sh scan -v    \
-          -ProjectName "${PROJECT_NAME}"    \
-          -CxServer 'https://checkmarx.security.ge.com'     \
-          -CxUser '$CHECKMARX_USER'   \
-          -CxPassword '$CHECKMARX_PASSWORD'   \
-          -preset 'Default 2014'    \
-          -locationtype folder      \
-          -locationpath '$WORKSPACE'
-        """
+        def CHECKMARX_TEAM = /CxServer\\SP\\GE\\GE_PowerWater\\mdi_12782/
+        def CHECKMARX_APP = "\\${config.appName}-${env.BRANCH_NAME}"
+        def PROJECT_NAME = "${CHECKMARX_TEAM}${CHECKMARX_APP}"
+        // checkout([$class: 'GitSCM', branches: [[name: "${branch}"]], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'MessageExclusion', excludedMessage: '.*\\[skip-ci\\].*'], [$class: 'LocalBranch', localBranch: "${branch}"]], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'ge-git', url: "${config.cloneAppUrl}"]]])
+
+        withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: 'ge-checkmarx',
+                                   usernameVariable: 'CHECKMARX_USER', passwordVariable: 'CHECKMARX_PASSWORD']]) {
+          sh """#!/bin/bash
+            echo Performing /opt/CxConsolePlugin/runCxConsole.sh scan -v    \
+            -ProjectName "${PROJECT_NAME}"    \
+            -CxServer 'https://checkmarx.security.ge.com'     \
+            -CxUser '$CHECKMARX_USER'   \
+            -CxPassword '$CHECKMARX_PASSWORD'   \
+            -preset 'Default 2014'    \
+            -locationtype folder      \
+            -locationpath '$WORKSPACE/scm'
+            /opt/CxConsolePlugin/runCxConsole.sh scan -v    \
+            -ProjectName "${PROJECT_NAME}"    \
+            -CxServer 'https://checkmarx.security.ge.com'     \
+            -CxUser '$CHECKMARX_USER'   \
+            -CxPassword '$CHECKMARX_PASSWORD'   \
+            -preset 'Default 2014'    \
+            -locationtype folder      \
+            -locationpath '$WORKSPACE/scm'
+          """
+        }
       }
 
     }
