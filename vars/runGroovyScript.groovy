b23f78d824d6ac12e1c06492859416b6792216c2
Simon Townsend <simon.townsend1@ge.com>
2017-04-19 11:45:44 -0500

    meh

--- vars/runGroovyScript.groovy
+++ vars/runGroovyScript.groovy
@@ -1,9 +1,10 @@
 
 
 def call(scriptFile, config) {
-  def binding = new Binding();
-  binding.setVariable('sh', sh);
-  binding.setVariable('config', config)
+  def binding = new Binding()
+  def myShell = sh
+  binding.setProperty('sh', myShell);
+  binding.setProperty('config', config)
   GroovyShell shell = new GroovyShell(binding);
   shell.evaluate(scriptFile);
 }
