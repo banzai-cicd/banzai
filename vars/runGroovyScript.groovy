80cc6695b52c2b0d18c1ef9817601f55f5494824
Simon Townsend <simon.townsend1@ge.com>
2017-04-19 13:46:52 -0500

    hm...

--- vars/runGroovyScript.groovy
+++ vars/runGroovyScript.groovy
@@ -1,27 +1,11 @@
-import org.codehaus.groovy.control.CompilerConfiguration
+import groovy.lang.GroovyClassLoader
 
 def call(scriptFile, config) {
-  @NonCPS
-  def compilerConfiguration = new CompilerConfiguration()
-  compilerConfiguration.scriptBaseClass = DelegatingScript.class.name
-  // Configure the GroovyShell and pass the compiler configuration.
-  def binding = new Binding()
-  binding.setVariable('config', config)
-  def shell = new GroovyShell(this.class.classLoader, binding, compilerConfiguration)
-
-  // get an instance of our current class as it should have all the methods we want to call from the script
-  def instanceOfThis = this.getClass().newInstance()
-
-  // parse the scriptFile
-  def script = shell.parse(scriptFile)
-  // set the delegate of our script file to an instance of the class we're currently in.
-  script.setDelegate(instanceOfThis)
-
-  // def binding = new Binding()
-  // binding.setVariable('config', config)
-  // GroovyShell shell = new GroovyShell(binding);
   try {
-    script.run();
+    GroovyClassLoader loader = new GroovyClassLoader(getClass().getClassLoader());
+    Class groovyClass = loader.parseClass(scriptFile);
+		BuildScript buildScript = (BuildScript) groovyClass.newInstance();
+    buildScript.run(&sh);
   } catch (GroovyRuntimeException e) {
     throw e.getCause()
   }
