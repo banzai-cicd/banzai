10d9e10105068972e739ad9ca5af0e3f74952363
Simon Townsend <simon.townsend1@ge.com>
2017-04-19 13:48:56 -0500

    test

--- vars/runGroovyScript.groovy
+++ vars/runGroovyScript.groovy
@@ -2,10 +2,10 @@ import groovy.lang.GroovyClassLoader
 
 def call(scriptFile, config) {
   try {
-    GroovyClassLoader loader = new GroovyClassLoader(getClass().getClassLoader());
-    Class groovyClass = loader.parseClass(scriptFile);
-		BuildScript buildScript = (BuildScript) groovyClass.newInstance();
-    buildScript.run(&sh);
+    GroovyClassLoader loader = new GroovyClassLoader(getClass().getClassLoader())
+    Class groovyClass = loader.parseClass(scriptFile)
+		BuildScript buildScript = (BuildScript) groovyClass.newInstance()
+    buildScript.run(&sh)
   } catch (GroovyRuntimeException e) {
     throw e.getCause()
   }
