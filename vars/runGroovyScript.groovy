110937881a515e0ce330ee3e4a16f813156690ce
Simon Townsend <simon.townsend1@ge.com>
2017-04-19 13:57:38 -0500

    test untyped

--- vars/runGroovyScript.groovy
+++ vars/runGroovyScript.groovy
@@ -4,7 +4,7 @@ def call(scriptFile, config) {
   try {
     GroovyClassLoader loader = new GroovyClassLoader(getClass().getClassLoader())
     Class groovyClass = loader.parseClass(scriptFile)
-		BuildScript buildScript = (BuildScript) groovyClass.newInstance()
+		def buildScript = groovyClass.newInstance()
     buildScript.invokeMethod("run", this.&sh)
   } catch (GroovyRuntimeException e) {
     throw e.getCause()
