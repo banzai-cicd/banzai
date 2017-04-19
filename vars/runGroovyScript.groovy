e9d3d0bd9136757dd320dadf7d3fc7f9cdbd6ccb
Simon Townsend <simon.townsend1@ge.com>
2017-04-19 13:23:22 -0500

    no way this works

--- vars/runGroovyScript.groovy
+++ vars/runGroovyScript.groovy
@@ -1,11 +1,26 @@
 
 
 def call(scriptFile, config) {
+  def compilerConfiguration = new CompilerConfiguration()
+  compilerConfiguration.scriptBaseClass = DelegatingScript.class.name
+  // Configure the GroovyShell and pass the compiler configuration.
   def binding = new Binding()
   binding.setVariable('config', config)
-  GroovyShell shell = new GroovyShell(binding);
+  def shell = new GroovyShell(this.class.classLoader, binding, compilerConfiguration)
+
+  // get an instance of our current class as it should have all the methods we want to call from the script
+  def instanceOfThis = this.getClass().newInstance()
+
+  // parse the scriptFile
+  def script = shell.parse(scriptFile)
+  // set the delegate of our script file to an instance of the class we're currently in.
+  script.setDelegate(instanceOfThis)
+
+  // def binding = new Binding()
+  // binding.setVariable('config', config)
+  // GroovyShell shell = new GroovyShell(binding);
   try {
-    shell.evaluate(scriptFile);
+    script.run();
   } catch (GroovyRuntimeException e) {
     throw e.getCause()
   }
