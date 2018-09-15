021069cb980460e75c0b7f005cb90652a451b41f
502061514 <502061514@G358J4Q2E.logon.ds.ge.com>
2018-09-15 14:10:50 -0400

    refactoring

--- vars/runPromote.groovy
+++ vars/runPromote.groovy
@@ -1,5 +1,38 @@
 #! /usr/bin/env groovy
 
+@NonCPS
+def updateStackYamlVersion(versionYmlData, stackYmlData) {
+	versionYmlData.version.each{key, value ->
+		if (stackYmlData.services.containsKey(key))	{
+			def existingImgName = stackYmlData.services[key].image
+			def existingImgVersion = existingImgName.split(/:/)[-1]
+			if(!(existingImgVersion.toLowerCase().contains('.com'))) {  // Ignore if no tag present with image name
+				def newImgVersion = value
+				def newImgName = stackYmlData.services[key].image.replaceAll(existingImgVersion, newImgVersion)
+				stackYmlData.services[key].image = newImgName
+			}
+		}
+	}
+	/*def changeLogSets = currentBuild.changeSets
+	for (int i = 0; i < changeLogSets.size(); i++) {
+		def entries = changeLogSets[i].items
+		for (int j = 0; j < entries.length; j++) {
+			def entry = entries[j]
+			def files = new ArrayList(entry.affectedFiles)
+			for (int k = 0; k < files.size(); k++) {
+				def file = files[k]
+				if (file.path.contains('dev/')) {
+					dev_modified = true
+				} else if (file.path.contains('qa/')) {
+					qa_modified = true
+				} else if (file.path.contains('prod/')) {
+					prod_modified = true
+				}
+			}
+		}
+	}*/
+}
+
 def call(config, environment) {
 	
 	paramList = []
@@ -8,7 +41,6 @@ def call(config, environment) {
 	node(){
 		sshagent (credentials: config.sshCreds) {
 			stage ("Preparing for Deployment") {
-			  //runDeploy(config, 'QA') // Add QA param
 				
 				deploymntRepoName =  config.promoteRepo.tokenize('/').last().split("\\.")[0]
 				echo "deploymntRepoName: ${deploymntRepoName}"
@@ -19,7 +51,7 @@ def call(config, environment) {
 				stackYmlData = readYaml file: "${WORKSPACE}/${deploymntRepoName}/envs/${environment}/${config.stackName}-dev.yml"
 				versionYmlData = readYaml file: "${WORKSPACE}/${deploymntRepoName}/envs/${environment}/version.yml"
 				
-				versionYmlData.version.each{key, value ->
+				/*versionYmlData.version.each{key, value ->
 					if (stackYmlData.services.containsKey(key))	{
 						existingImgName = stackYmlData.services[key].image						
 						existingImgVersion = existingImgName.split(/:/)[-1]
@@ -29,7 +61,8 @@ def call(config, environment) {
 							stackYmlData.services[key].image = newImgName
 						}
 					}									
-				}
+				}*/
+				updateStackYamlVersion(versionYmlData, stackYmlData)
 				echo "Stack Yml Data after updating from Version file: ${stackYmlData.toMapString()}"
 				
 				stackYmlData.services.each{ serviceName,value ->
@@ -60,11 +93,11 @@ def call(config, environment) {
 					   if(existingImgVersion.toLowerCase().contains('.com')) {
 						   existingImgVersion = ''
 					   }
-					   echo ("VERSION_INFO Class: "+VERSION_INFO.getClass())
+					   
 					   newImgVersion = VERSION_INFO[serviceName]
 					   newImgName = stackYmlData.services[serviceName].image.replaceAll(existingImgVersion, newImgVersion)
 					   sh "yaml w -i '${WORKSPACE}/${deploymntRepoName}/envs/${environment}/${config.stackName}-dev.yml' services.${serviceName}.image ${newImgName}"
-					   echo ("Updating YAML Service: ${serviceName} Version: "+stackYmlData.services[serviceName].image)
+					   echo "Updating YAML Service: ${serviceName} Version: "+stackYmlData.services[serviceName].image
 				   }
 				}
 			   
