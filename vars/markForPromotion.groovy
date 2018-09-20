2f761b612e872b805a4e09c72a4436552f7f506e
502061514 <502061514@G358J4Q2E.logon.ds.ge.com>
2018-09-20 13:16:06 -0400

    fix

--- vars/markForPromotion.groovy
+++ vars/markForPromotion.groovy
@@ -22,19 +22,18 @@ def call(config) {
 		throw new IllegalArgumentException("no ${WORKSPACE}/versionInfo exists!")
 	  }
 	  def versionInfo = versionFileInfo.text
-	  def BUILD_VERSION_QA = versionInfo.tokenize('|')[1]
+	  def BUILD_VERSION_QA = ''
+	  
+	  if(versionInfo.toLowerCase().contains('|')) {
+		  BUILD_VERSION_QA = versionInfo.tokenize('|')[1]
+	  } else {
+		  BUILD_VERSION_QA = versionInfo
+	  }
+	  
 	  def deploymntRepoName =  config.deploymentRepo.tokenize('/').last().split("\\.")[0]
 	  
 	  echo "MARK-PROMOTION SCRIPT - Updating deploymntRepo for ${config.stackServiceName} module with version ${BUILD_VERSION_QA}"
-	  /*sh """#!/bin/bash
-		pwd
-		rm -rf "$WORKSPACE"/${deploymntRepoName}
-		git ${config.deploymentRepo}		
-		yaml w -i "$WORKSPACE"/${deploymntRepoName}/envs/qa/version.yml version.${config.stackServiceName} ${BUILD_VERSION_QA}
-		git -C ${deploymntRepoName} commit -a -m 'Updated QA version for ${config.stackServiceName} module' || true
-		git -C ${deploymntRepoName} pull && git -C ${deploymntRepoName} push origin master
-	  """    		*/
-	  sh "pwd"
+	  
 	  sh "rm -rf ${deploymntRepoName}"
 	  sh "git clone ${config.deploymentRepo}"
 	  sh "yaml w -i ${deploymntRepoName}/envs/qa/version.yml version.${config.stackServiceName} ${BUILD_VERSION_QA}"
