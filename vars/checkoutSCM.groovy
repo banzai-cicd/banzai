2b1fe2bab6ca847ffd556cc5012fb7cb3beeb1a3
Ganapathi, Ramesh <Ramesh.Ganapathi@ge.com>
2018-11-06 10:33:56 -0500

    Commented Simon changes for using ssh

--- vars/checkoutSCM.groovy
+++ vars/checkoutSCM.groovy
@@ -3,10 +3,10 @@
 def call(config) {
 
   stage('Checkout') {
-    // checkout scm
+    checkout scm
 
     // this is dumb but ge github non-ssh url access is failing.
-    tokens = "${env.JOB_NAME}".tokenize('/')
+    /*tokens = "${env.JOB_NAME}".tokenize('/')
     org = tokens[0]
     repo = tokens[1]
     branch = tokens[2]
@@ -14,6 +14,6 @@ def call(config) {
     git branch: '${branch}',
       credentialsId: config.gitCredId,
       url: "ssh://git@github.build.ge.com:${org}/${repo}.git"
-  }
+  }*/
 
 }
