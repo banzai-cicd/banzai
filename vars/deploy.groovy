9de785d735efb4d45859503717ab2d2c1e086828
Ganapathi, Ramesh <Ramesh.Ganapathi@ge.com>
2018-11-14 15:07:47 -0500

    Update deploy.groovy

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -4,19 +4,17 @@ import java.util.regex.Matcher
 import java.util.regex.Pattern
 
 def call(config) {
-  node{
-	  stage ('Deploy') {
-	
-	    if (config.deployBranches) {
-	      Pattern pattern = Pattern.compile(config.deployBranches)
-	
-	      if (!(BRANCH_NAME ==~ pattern)) {
-	        println "${BRANCH_NAME} does not match the deployBranchesPattern pattern. Skipping Deploy"
-	        return
-	      }
-	    }	
-		//runDeploy(config,'DEV')
-		echo "Deployed to DEV!"
-	  }
-   }
+  stage ('Deploy') {
+
+    if (config.deployBranches) {
+      Pattern pattern = Pattern.compile(config.deployBranches)
+
+      if (!(BRANCH_NAME ==~ pattern)) {
+        println "${BRANCH_NAME} does not match the deployBranchesPattern pattern. Skipping Deploy"
+        return
+      }
+    }
+
+    runDeploy(config)
+  }
 }
