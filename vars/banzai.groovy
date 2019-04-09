9d6994fb391be41b6602dea00d2cce3ace993c4e
Simon Townsend <simon.townsend1@ge.com>
2019-04-09 11:24:15 -0500

    initial commit of downsteamBuilds support

--- vars/banzai.groovy
+++ vars/banzai.groovy
@@ -1,225 +1,231 @@
 #!/usr/bin/env groovy
 
 def call(body) {
-  // evaluate the body block, and collect configuration into the object
-  def config = [:]
-  body.resolveStrategy = Closure.DELEGATE_FIRST
-  body.delegate = config
-  body()
+    // evaluate the body block, and collect configuration into the object
+    def config = [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = config
+    body()
 
-  if (config.throttle) {
-	throttle (config.throttle.tokenize(',')) {
-	  runPipeline(config)
-	}
-  } else {
-	runPipeline(config)
-  }
+    if (config.throttle) {
+        throttle(config.throttle.tokenize(',')) {
+            runPipeline(config)
+        }
+    } else {
+        runPipeline(config)
+    }
 }
 
 def runPipeline(config) {
-  env.GITHUB_API_URL = 'https://github.build.ge.com/api/v3'
+    env.GITHUB_API_URL = 'https://github.build.ge.com/api/v3'
 
-  /*
-	Determine the total number of steps in the pipeline that are activated
-	Jenkins Pipelines don't allow many groovy methods (CPS issues) like .findAll...hence the nastiness
-  */
-  def steps = []
-  for (entry in [!config.skipSCM, config.sast, config.build, config.publish, config.deploy, config.integrationTests, config.markForPromotion, config.promote]) {
-	if (entry == true) { steps.push(entry) }
-  }
-  def passedSteps = 0
+    /*
+      Determine the total number of steps in the pipeline that are activated
+      Jenkins Pipelines don't allow many groovy methods (CPS issues) like .findAll...hence the nastiness
+    */
+    def steps = []
+    for (entry in [!config.skipSCM, config.sast, config.build, config.publish, config.deploy, config.integrationTests, config.markForPromotion, config.promote]) {
+        if (entry == true) {
+            steps.push(entry)
+        }
+    }
+    def passedSteps = 0
 
-  def passStep = { step ->
-	passedSteps += 1
-	logger "BANZAI: ${step} PASSED : ${passedSteps}/${steps.size()} STEPS COMPLETE"
-	if (passedSteps >= steps.size()) {
-	  currentBuild.result = 'SUCCESS'
-	}
-  }
+    def passStep = { step ->
+        passedSteps += 1
+        logger "BANZAI: ${step} PASSED : ${passedSteps}/${steps.size()} STEPS COMPLETE"
+        if (passedSteps >= steps.size()) {
+            currentBuild.result = 'SUCCESS'
+        }
+    }
 
-  def isGithubError = { err ->
-	return err.message.contains("The suplied credentials are invalid to login") ? true : false;
-  }
+    def isGithubError = { err ->
+        return err.message.contains("The suplied credentials are invalid to login") ? true : false
+    }
 
-  node() {
-	// clean up old builds (experimental, not sure if this is actually working or not. time will tell)
-	properties([buildDiscarder(logRotator(artifactDaysToKeepStr: '', artifactNumToKeepStr: '', daysToKeepStr: '5', numToKeepStr: '10'))])
-	
-	// support for jenkins 'tools'
-	if (config.jdk) {
-	  jdk = tool name: config.jdk
-	  env.JAVA_HOME = "${jdk}"
+    node() {
+        // clean up old builds (experimental, not sure if this is actually working or not. time will tell)
+        properties([buildDiscarder(logRotator(artifactDaysToKeepStr: '', artifactNumToKeepStr: '', daysToKeepStr: '5', numToKeepStr: '10'))])
 
-    logger "JAVA_HOME: ${jdk}"
-	}
-	
-	if (config.node) {
-	  def nodeVersion = "node ${config.node}"
-	  env.NODEJS_HOME = "${tool nodeVersion}"
-	  // on linux / mac
-	  env.PATH="${env.NODEJS_HOME}/bin:${env.PATH}"
-	}
+        // support for jenkins 'tools'
+        if (config.jdk) {
+            jdk = tool name: config.jdk
+            env.JAVA_HOME = "${jdk}"
 
-	sshagent (credentials: config.sshCreds) {
-	  // TODO notify Flowdock build starting
-	  echo "My branch is: ${env.BRANCH_NAME}"
+            logger "JAVA_HOME: ${jdk}"
+        }
 
-	  // checkout the branch that triggered the build if not explicitly skipped
-	  if (config.preCleanup) {
-		logger "Starting Fresh"
-		step([$class: 'WsCleanup'])
-	  }
+        if (config.node) {
+            def nodeVersion = "node ${config.node}"
+            env.NODEJS_HOME = "${tool nodeVersion}"
+            // on linux / mac
+            env.PATH = "${env.NODEJS_HOME}/bin:${env.PATH}"
+        }
 
-	  if (!config.skipSCM) {
-		try {
-		  notify(config, 'Checkout', 'Pending', 'PENDING')
-		  checkoutSCM(config)
-		  passStep('CHECKOUT')
-		  notify(config, 'Checkout', 'Successful', 'SUCCESS')
-		} catch (err) {
-		  echo "Caught: ${err}"
-		  currentBuild.result = 'UNSTABLE'
-		  if (isGithubError(err)) {
-			notify(config, 'Checkout', 'githubdown', 'FAILURE', true)
-		  } else {
-			notify(config, 'Checkout', 'Failed', 'FAILURE')
-		  }
-		  throw err
-		}
-	  }
+        sshagent(credentials: config.sshCreds) {
+            // TODO notify Flowdock build starting
+            echo "My branch is: ${env.BRANCH_NAME}"
 
-	  if (config.sast) {
-		try {
-		  notify(config, 'SAST', 'Pending', 'PENDING')
-		  sast(config)
-		  passStep('SAST')
-		  notify(config, 'SAST', 'Successful', 'SUCCESS')
-		} catch (err) {
-		  echo "Caught: ${err}"
-		  currentBuild.result = 'UNSTABLE'
-		  notify(config, 'Build', 'Failed', 'FAILURE')
-		  throw err
-		}
-	  }
+            // checkout the branch that triggered the build if not explicitly skipped
+            if (config.preCleanup) {
+                logger "Starting Fresh"
+                step([$class: 'WsCleanup'])
+            }
 
-	  if (config.build) {
-		try {
-		  notify(config, 'Build', 'Pending', 'PENDING')
-		  build(config)
-		  passStep('BUILD')
-		  notify(config, 'Build', 'Successful', 'SUCCESS')
-		} catch (err) {
-		  echo "Caught: ${err}"
-		  currentBuild.result = 'FAILURE'
-		  if (isGithubError(err)) {
-			notify(config, 'Build', 'githubdown', 'FAILURE', true)
-		  } else {
-			notify(config, 'Build', 'Failed', 'FAILURE')
-		  }
-		  throw err
-		}
-	  }
+            if (!config.skipSCM) {
+                try {
+                    notify(config, 'Checkout', 'Pending', 'PENDING')
+                    checkoutSCM(config)
+                    passStep('CHECKOUT')
+                    notify(config, 'Checkout', 'Successful', 'SUCCESS')
+                } catch (err) {
+                    echo "Caught: ${err}"
+                    currentBuild.result = 'UNSTABLE'
+                    if (isGithubError(err)) {
+                        notify(config, 'Checkout', 'githubdown', 'FAILURE', true)
+                    } else {
+                        notify(config, 'Checkout', 'Failed', 'FAILURE')
+                    }
+                    throw err
+                }
+            }
 
-	  /*
-		all notify calls past the build stage will skip notifcations to github
-	  */
-	  if (config.publish) {
-		try {
-		  notify(config, 'Publish', 'Pending', 'PENDING', true)
-		  publish(config)
-		  passStep('PUBLISH')
-		  notify(config, 'Publish', 'Successful', 'SUCCESS', true)
-		} catch (err) {
-		  echo "Caught: ${err}"
-		  currentBuild.result = 'FAILURE'
-		  if (isGithubError(err)) {
-			notify(config, 'Publish', 'githubdown', 'FAILURE', true)
-		  } else {
-			notify(config, 'Publish', 'Failed', 'FAILURE', true)
-		  }
-		  throw err
-		}
-	  }
+            if (config.sast) {
+                try {
+                    notify(config, 'SAST', 'Pending', 'PENDING')
+                    sast(config)
+                    passStep('SAST')
+                    notify(config, 'SAST', 'Successful', 'SUCCESS')
+                } catch (err) {
+                    echo "Caught: ${err}"
+                    currentBuild.result = 'UNSTABLE'
+                    notify(config, 'Build', 'Failed', 'FAILURE')
+                    throw err
+                }
+            }
 
-	  if (config.deploy) {
-		try {
-		  notify(config, 'Deploy', 'Pending', 'PENDING', true)
-		  deploy(config)
-		  passStep('DEPLOY')
-		  notify(config, 'Deploy', 'Successful', 'SUCCESS', true)
-		  // TODO notify Flowdock
-		} catch (err) {
-		  echo "Caught: ${err}"
-		  currentBuild.result = 'FAILURE'
-		  notify(config, 'Deploy', 'Failed', 'FAILURE', true)
-		  throw err
-		}
-	  }
+            if (config.build) {
+                try {
+                    notify(config, 'Build', 'Pending', 'PENDING')
+                    build(config)
+                    passStep('BUILD')
+                    notify(config, 'Build', 'Successful', 'SUCCESS')
+                } catch (err) {
+                    echo "Caught: ${err}"
+                    currentBuild.result = 'FAILURE'
+                    if (isGithubError(err)) {
+                        notify(config, 'Build', 'githubdown', 'FAILURE', true)
+                    } else {
+                        notify(config, 'Build', 'Failed', 'FAILURE')
+                    }
+                    throw err
+                }
+            }
 
-	  if (config.integrationTests) {
-		try {
-		  notify(config, 'IT', 'Pending', 'PENDING', true)
+            /*
+              all notify calls past the build stage will skip notifcations to github
+            */
+            if (config.publish) {
+                try {
+                    notify(config, 'Publish', 'Pending', 'PENDING', true)
+                    publish(config)
+                    passStep('PUBLISH')
+                    notify(config, 'Publish', 'Successful', 'SUCCESS', true)
+                } catch (err) {
+                    echo "Caught: ${err}"
+                    currentBuild.result = 'FAILURE'
+                    if (isGithubError(err)) {
+                        notify(config, 'Publish', 'githubdown', 'FAILURE', true)
+                    } else {
+                        notify(config, 'Publish', 'Failed', 'FAILURE', true)
+                    }
+                    throw err
+                }
+            }
 
-		  if (config.xvfb) {
-			def screen = config.xvfbScreen ?: '1800x900x24';
+            if (config.deploy) {
+                try {
+                    notify(config, 'Deploy', 'Pending', 'PENDING', true)
+                    deploy(config)
+                    passStep('DEPLOY')
+                    notify(config, 'Deploy', 'Successful', 'SUCCESS', true)
+                    // TODO notify Flowdock
+                } catch (err) {
+                    echo "Caught: ${err}"
+                    currentBuild.result = 'FAILURE'
+                    notify(config, 'Deploy', 'Failed', 'FAILURE', true)
+                    throw err
+                }
+            }
 
-			wrap([$class: 'Xvfb', screen: screen]) {
-			  integrationTests(config)
-			}
-		  } else {
-			integrationTests(config)
-		  }
+            if (config.integrationTests) {
+                try {
+                    notify(config, 'IT', 'Pending', 'PENDING', true)
 
-		  passStep('IT')
-		  notify(config, 'IT', 'Successful', 'SUCCESS', true)
-		} catch (err) {
-		  echo "Caught: ${err}"
-		  currentBuild.result = 'FAILURE'
-		  notify(config, 'IT', 'Failed', 'FAILURE', true)
-		  throw err
-		}
-	  }
-	  
-	  if (config.markForPromotion) {
-		  try {
-			notify(config, 'MarkForPromotion', 'Pending', 'PENDING', true)
-			markForPromotion(config)
-			passStep('MARK FOR PROMOTION')
-			notify(config, 'MarkForPromotion', 'Successful', 'SUCCESS', true)
-		  } catch (err) {
-			echo "Caught: ${err}"
-			currentBuild.result = 'FAILURE'
-			notify(config, 'MarkForPromotion', 'Failed', 'FAILURE', true)
-			throw err
-		  }
-	  }
-	  
-	  if (config.postCleanup) {
-		  logger "Cleaning up"
-		  step([$class: 'WsCleanup'])
-	  }
-	} // ssh-agent
-	} // node	
-	
-	if (config.promote) {
-		try {
-		  notify(config, 'Promote', 'Pending', 'PENDING', true)
-		  promote(config)
-		  passStep('PROMOTE')
-		  notify(config, 'Promote', 'Successful', 'SUCCESS', true)
-		  // TODO notify Flowdock
-		} catch (err) {
-		  echo "Caught: ${err}"
-		  currentBuild.result = 'FAILURE'
-		  notify(config, 'Promote', 'Failed', 'FAILURE', true)
-		  throw err
-		}
-	}
-	
-	 /*node() {
-	  if (config.postCleanup) {
-		logger "Cleaning up"
-		step([$class: 'WsCleanup'])
-		  }
-	  }*/ // node
+                    if (config.xvfb) {
+                        def screen = config.xvfbScreen ?: '1800x900x24'
+
+                        wrap([$class: 'Xvfb', screen: screen]) {
+                            integrationTests(config)
+                        }
+                    } else {
+                        integrationTests(config)
+                    }
+
+                    passStep('IT')
+                    notify(config, 'IT', 'Successful', 'SUCCESS', true)
+                } catch (err) {
+                    echo "Caught: ${err}"
+                    currentBuild.result = 'FAILURE'
+                    notify(config, 'IT', 'Failed', 'FAILURE', true)
+                    throw err
+                }
+            }
+
+            if (config.markForPromotion) {
+                try {
+                    notify(config, 'MarkForPromotion', 'Pending', 'PENDING', true)
+                    markForPromotion(config)
+                    passStep('MARK FOR PROMOTION')
+                    notify(config, 'MarkForPromotion', 'Successful', 'SUCCESS', true)
+                } catch (err) {
+                    echo "Caught: ${err}"
+                    currentBuild.result = 'FAILURE'
+                    notify(config, 'MarkForPromotion', 'Failed', 'FAILURE', true)
+                    throw err
+                }
+            }
+
+            if (config.postCleanup) {
+                logger "Cleaning up"
+                step([$class: 'WsCleanup'])
+            }
+
+            if (config.downstreamBuilds) {
+                downstreamBuilds(config)
+            }
+        } // ssh-agent
+    } // node
+
+    if (config.promote) {
+        try {
+            notify(config, 'Promote', 'Pending', 'PENDING', true)
+            promote(config)
+            passStep('PROMOTE')
+            notify(config, 'Promote', 'Successful', 'SUCCESS', true)
+            // TODO notify Flowdock
+        } catch (err) {
+            echo "Caught: ${err}"
+            currentBuild.result = 'FAILURE'
+            notify(config, 'Promote', 'Failed', 'FAILURE', true)
+            throw err
+        }
+    }
+
+    /*node() {
+     if (config.postCleanup) {
+       logger "Cleaning up"
+       step([$class: 'WsCleanup'])
+         }
+     }*/ // node
 }
---
+++ vars/downstreamBuilds.groovy
@@ -0,0 +1,45 @@
+#!/usr/bin/env groovy
+
+import java.util.regex.Pattern
+java.util.regex.Matcher
+
+
+String determineOrgName(url) {
+    def finder = (url =~ /:([^:]*)\//)
+
+    return finder.getAt(0).getAt(1))
+}
+
+String determineRepoName(url) {
+    return scm.getUserRemoteConfigs()[0].getUrl().tokenize('/').last().split("\\.")[0]
+}
+
+def call(config) {
+    stage ('Downstream Builds') {
+
+        if (config.downstreamBuildBranches) {
+            Pattern pattern = Pattern.compile(config.downstreamBuildBranches)
+
+            if (!(BRANCH_NAME ==~ pattern)) {
+                logger "${BRANCH_NAME} does not match the downstreamBuildBranches pattern. Skipping Downstream Builds"
+                return
+            }
+        }
+
+        withCredentials([string(credentialsId: config.gitCredId, variable: 'TOKEN')]) {
+            def url = scm.getUserRemoteConfigs()[0].getUrl()
+            def orgName = determineOrgName(url)
+            def repoName = determineRepoName(url)
+
+            def branchInfoUrl = "https://github.build.ge.com/api/v3/repos/${orgName}/${repoName}/branches/${BRANCH_NAME}"
+            def branchInfoResponse = httpRequest(url: branchInfoUrl, customHeaders: [[Authorization: "token ${TOKEN}"]])
+            Map branchInfo = (Map) new JsonSlurper().parseText(response.content)
+            def latestCommit = branchInfo.commit.sha
+            logger "latest commit: ${latestCommit}"
+        }
+        
+        // getLatestCommitHash  commit.sha
+        // lookupClosedPrWithMatchingMergeHash https://github.build.ge.com/api/v3/repos/PowerApi/power-api/pulls?state=closed [*].merge_commit_sha
+        // checkForDownstreamBuildLabels result.labels
+    }
+}
