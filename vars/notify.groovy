289c6b01f1dca95488a29bc10571d8e9e82ee6b6
Simon Townsend <simon.townsend1@ge.com>
2019-04-15 09:20:52 -0500

    re-enable flowdock and update README

--- vars/notify.groovy
+++ vars/notify.groovy
@@ -2,11 +2,6 @@
 
 def call(config, stage, message, status, skipGit=false) {
   if (config.flowdock) {
-    //notifyFlowdock(config, stage, message)
+    notifyFlowdock(config, stage, message)
   }
-
-  if (!skipGit) {
-    //notifyGit(config, "${stage} ${message}", status)
-  }
-
 }
--- vars/notifyGit.groovy
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env groovy
-
-def call(config, msg, status) {
-  //githubNotify description: msg,  status: status, credentialsId: config.gitCredId, account: config.gitAccount, gitApiUrl: GITHUB_API_URL
-}
