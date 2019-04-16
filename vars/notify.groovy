c4079806e053d30de4326d94f1e8e480d6d0fa30
Simon Townsend <simon.townsend1@ge.com>
2019-04-16 07:54:57 -0500

    add back notifyGit step

--- vars/notify.groovy
+++ vars/notify.groovy
@@ -4,4 +4,9 @@ def call(config, stage, message, status, skipGit=false) {
   if (config.flowdock) {
     notifyFlowdock(config, stage, message)
   }
+
+  if (notifyGit != false) {
+    notifyGit(config, "${stage} ${message}", status)
+  }
+
 }
