95f187ea170b3b6f2b537aab0f39f7613414f86d
Simon Townsend <simon.townsend1@ge.com>
2019-04-17 09:46:54 -0500

    filterSecretsStep bugfix

--- vars/filterSecretsStep.groovy
+++ vars/filterSecretsStep.groovy
@@ -4,14 +4,14 @@ def call(config) {
 
   if (config.filterSecrets) {
     // determine if branch matches and filterSecrets branches
-    def secretConfigKey = filterSecrets.keySet().find { it ==~ BRANCH_NAME }
+    def secretConfigKey = config.filterSecrets.keySet().find { it ==~ BRANCH_NAME }
     if (!secretConfigKey) {
         logger "filterSecrets does not contain an entry that matches the branch: ${BRANCH_NAME}"
         return
     }
 
     notify(config, 'Filter Secrets', 'Pending', 'PENDING')
-    def secretConfig = filterSecrets[secretConfigKey]
+    def secretConfig = config.filterSecrets[secretConfigKey]
     filterSecrets(secretConfig)
     notify(config, 'Filter Secrets', 'Successful', 'SUCCESS')
   }
