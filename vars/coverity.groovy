0090ee3745d2b519d11532984223ce6d70082e1c
Simon Townsend <simon.townsend1@ge.com>
2019-04-23 15:01:40 -0500

    add coverity var

---
+++ vars/coverity.groovy
@@ -0,0 +1,42 @@
+#!/usr/bin/env groovy
+
+def call(config, opts) {
+    def STREAM_NAME = opts.streamName ?: "${config.appName}-${env.BRANCH_NAME}"
+    def buildCmd = BUILD_CMD ?: opts.buildCmd
+
+    if (!opts.credId) {
+        logger "Coverity: No credId specified: Skipping"
+        return
+    }
+    
+    withCredentials([file(credentialsId: opts.credId, variable: 'CRED_FILE')]) {
+      synopsys_coverity buildCommand: 'mvn clean install', 
+                buildStatusForIssues: 'SUCCESS', 
+                changeSetExclusionPatterns: '', 
+                changeSetInclusionPatterns: '', 
+                checkForIssuesInView: false, 
+                commands: [
+                        [command: "cov-build --dir ${WORKSPACE}/idir ${buildCmd}"], 
+                        [command: "cov-analyze --dir ${WORKSPACE}/idir"], 
+                        [command: "cov-commit-defects --dir ${WORKSPACE}/idir --host ${COVERITY_HOST} --https-port ${COVERITY_PORT} --stream ${COV_STREAM} —auth-key-file=${CRED_FILE} --on-new-cert trust"]], 
+                configureChangeSetPatterns: false, 
+                coverityAnalysisType: 'COV_ANALYZE', 
+                coverityRunConfiguration: 'ADVANCED', 
+                coverityToolName: 'default', 
+                onCommandFailure: 'SKIP_REMAINING_COMMANDS', 
+                projectName: opts.projectName, 
+                streamName: STREAM_NAME, 
+                viewName: ''
+    }
+
+    if (opts.resultEmails) {
+        opts.resultEmails.each {
+                logger "Emailing Coverity Scan Results to ${it}"
+                emailext attachmentsPattern: "${WORKSPACE}/idir/output/summary.txt", body: "BUILD_URL: ${BUILD_URL}", 
+                        subject: "Coverity Scan Summary: ${env.JOB_NAME} - Build # ${env.BUILD_NUMBER}", 
+                        to: it 
+                logger "Sent Coverity Scan Results..."
+        }
+        
+    }
+}
