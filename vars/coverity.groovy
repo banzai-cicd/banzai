2f3311d82895e5b3a29f46c2740d11bc8be93628
Simon Townsend <simon.townsend1@ge.com>
2019-04-24 11:40:31 -0500

    fix for checking if coverity stream exists prior to running synopsys_coverity step

--- vars/coverity.groovy
+++ vars/coverity.groovy
@@ -6,6 +6,18 @@ def call(config, opts) {
     def iDir = "${env.WORKSPACE}/idir"
     
     // TODO: check all required params
+    if (!opts.serverHost) {
+        error("serverHost is required for Coverity")
+        return
+    }
+    if (!opts.serverPort) {
+        error("serverPort is required for Coverity")
+        return
+    }
+    if (!opts.toolId) {
+        error("toolId is required for Coverity")
+        return
+    }
     if (!opts.credId) {
         error("credId is required for Coverity")
         return
@@ -23,30 +35,50 @@ def call(config, opts) {
     sh "if [ -e ${iDir} ]; then rm -rf ${iDir} ; fi"
     
     withCredentials([file(credentialsId: opts.credId, variable: 'CRED_FILE')]) {
-      
-      // The Coverity plugin provides some variables at runtime to the commands that you specify. Hilariously, the Coverity team decided to use the same ${} syntax that Groovy
-      // uses for declaring the Coverity variables. So, we have to build our cov-commit-defects string using a combination of ${groovy} vars and escaped \${coverity} vars
-      def hostAndPort = "--host \${COVERITY_HOST} --https-port \${COVERITY_PORT}"
       def credParams = "--on-new-cert trust --auth-key-file ${CRED_FILE}"
-      def covManageCmd = "cov-manage-im --mode stream --add --set name:\${COV_STREAM} --set lang:mixed ${credParams} ${hostAndPort}"
+      // We have to first check and see if the stream exists since synopsys_coverity step doesn't allow us to react to cmd feedback.
+      // 1. check for the existence of the stream 
+      def coverityInstallDir = tool name: opts.toolId
+      def covManage = "${coverityInstallDir}/bin/cov-manage-im"
+      def listStreamsCmd = "unset https_proxy && ${covManage} --mode streams --show --name \"${streamName}\" --host ${opts.serverHost} --port ${opts.serverPort} --ssl ${credParams}"
+      logger "Executing: ${listStreamsCmd}"
+      def streamList = sh (
+          script: listStreamsCmd,
+          returnStdout: true
+        ).trim()
+      
+      def addStream = false
+      if (!streamList.contains(streamName)) {
+        addStream = true
+      }
+
+      // The synopsys_coverity steps provides some variables at runtime to the commands that you specify. Hilariously, the Coverity team decided to use the same ${} syntax that Groovy
+      // uses for declaring the Coverity variables. So, we have to build our cov-commit-defects string using a combination of ${groovy} vars and escaped \${coverity} vars      
+      def commands = []
+      if (addStream) {
+        def covAddStreamCmd = "cov-manage-im --mode streams --add --set name:\${COV_STREAM} --set lang:mixed ${credParams} --host \${COVERITY_HOST} --port \${COVERITY_PORT} --ssl"
+        def covBindStreamCmd = "cov-manage-im --mode projects --name ${opts.projectName} --update --insert stream:\${COV_STREAM} ${credParams} --host \${COVERITY_HOST} --port \${COVERITY_PORT} --ssl"
+        commands.addAll([covAddStreamCmd, covBindStreamCmd])
+      }
+
       def covBuildCmd = "cov-build --dir ${iDir} ${buildCmd}"
       def covAnalyzeCmd = "cov-analyze --dir ${iDir}"
-      def covCommitCmd = "cov-commit-defects --dir ${iDir} --stream \${COV_STREAM} ${credParams} ${hostAndPort}"
+      def covCommitCmd = "cov-commit-defects --dir ${iDir} --stream \${COV_STREAM} ${credParams} --host \${COVERITY_HOST} --https-port \${COVERITY_PORT}"
+      commands.addll([covBuildCmd, covAnalyzeCmd, covCommitCmd])
+      
+      // convert the list of command strings to a list of command objects
+      def formattedCommands = commands.collect { [command: it] }
 
       synopsys_coverity buildStatusForIssues: 'SUCCESS', 
                 changeSetExclusionPatterns: '', 
                 changeSetInclusionPatterns: '', 
                 checkForIssuesInView: false, 
-                commands: [
-                        [command: covManageCmd],
-                        [command: covBuildCmd], 
-                        [command: covAnalyzeCmd], 
-                        [command: covCommitCmd]], 
+                commands: formattedCommands, 
                 configureChangeSetPatterns: false, 
                 coverityAnalysisType: 'COV_ANALYZE', 
                 coverityRunConfiguration: 'ADVANCED', 
-                coverityToolName: 'default', 
-                onCommandFailure: 'SKIP_REMAINING_COMMANDS', 
+                coverityToolName: opts.toolId, 
+                onCommandFailure: 'EXECUTE_REMAINING_COMMANDS', 
                 projectName: opts.projectName, 
                 streamName: streamName, 
                 viewName: ''
