ecf816468e7b65b91ad78ecc6808b1996aa66e86
Simon Townsend <simon.townsend1@ge.com>
2019-04-24 08:16:06 -0500

    test fix for coverity host/port

--- vars/coverity.groovy
+++ vars/coverity.groovy
@@ -1,13 +1,18 @@
 #!/usr/bin/env groovy
 
 def call(config, opts) {
-    def STREAM_NAME = opts.streamName ?: "${config.appName}-${env.BRANCH_NAME}"
-    def buildCmd = env.BUILD_CMD ?: opts.buildCmd
-
+    // TODO: check all required params
     if (!opts.credId) {
         logger "Coverity: No credId specified: Skipping"
         return
     }
+
+    def STREAM_NAME = opts.streamName ?: "${config.appName}-${env.BRANCH_NAME}"
+    def buildCmd = env.BUILD_CMD ?: opts.buildCmd
+    def iDir = "${env.WORKSPACE}/idir"
+
+    // Clear the intermediate directory if it already exists
+    sh "if [ -e ${iDir} ]; then rm -rf ${iDir} ; fi"
     
     withCredentials([file(credentialsId: opts.credId, variable: 'CRED_FILE')]) {
       synopsys_coverity buildStatusForIssues: 'SUCCESS', 
@@ -15,9 +20,9 @@ def call(config, opts) {
                 changeSetInclusionPatterns: '', 
                 checkForIssuesInView: false, 
                 commands: [
-                        [command: "cov-build --dir ${WORKSPACE}/idir ${buildCmd}"], 
-                        [command: "cov-analyze --dir ${WORKSPACE}/idir"], 
-                        [command: "cov-commit-defects --dir ${WORKSPACE}/idir --host ${COVERITY_HOST} --https-port ${COVERITY_PORT} --stream ${COV_STREAM} —auth-key-file=${CRED_FILE} --on-new-cert trust"]], 
+                        [command: "cov-build --dir ${iDir} ${buildCmd}"], 
+                        [command: "cov-analyze --dir ${iDir}"], 
+                        [command: "cov-commit-defects --dir ${iDir} --host ${COVERITY_HOST} --https-port ${COVERITY_PORT} --stream ${STREAM_NAME} --on-new-cert trust —-auth-key-file ${CRED_FILE}"]], 
                 configureChangeSetPatterns: false, 
                 coverityAnalysisType: 'COV_ANALYZE', 
                 coverityRunConfiguration: 'ADVANCED', 
@@ -31,7 +36,7 @@ def call(config, opts) {
     if (opts.resultEmails) {
         opts.resultEmails.each {
                 logger "Emailing Coverity Scan Results to ${it}"
-                emailext attachmentsPattern: "${WORKSPACE}/idir/output/summary.txt", body: "BUILD_URL: ${BUILD_URL}", 
+                emailext attachmentsPattern: "**/idir/output/summary.txt", body: "BUILD_URL: ${env.BUILD_URL}", 
                         subject: "Coverity Scan Summary: ${env.JOB_NAME} - Build # ${env.BUILD_NUMBER}", 
                         to: it 
                 logger "Sent Coverity Scan Results..."
