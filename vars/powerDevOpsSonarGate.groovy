b67c88a32c7bf70f106c34b38c79a7b8718b60f7
Simon Townsend <simon.townsend1@ge.com>
2019-05-01 10:45:35 -0500

    conditionally turn off proxy for sonar based on no_proxy

--- vars/powerDevOpsSonarGate.groovy
+++ vars/powerDevOpsSonarGate.groovy
@@ -10,11 +10,12 @@ def call()
     def sqHelper = new SonarQubeHelper();
     PipelineSettings.SonarQubeSettings.initializeQualityMetrics();
     
-    if (PipelineSettings.ProxySettings.proxyHost && PipelineSettings.ProxySettings.proxyPort) {
-        logger "setting sonar proxy ${PipelineSettings.ProxySettings.proxyHost}:${PipelineSettings.ProxySettings.proxyPort}"
-        sqService.enableProxies(PipelineSettings.ProxySettings.proxyHost, PipelineSettings.ProxySettings.proxyPort);
+    if (!env.no_proxy || !env.no_proxy.contains(PipelineSettings.SonarQubeSettings.sonarHostUrl)) {
+        if (PipelineSettings.ProxySettings.proxyHost && PipelineSettings.ProxySettings.proxyPort) {
+            logger "setting sonar proxy ${PipelineSettings.ProxySettings.proxyHost}:${PipelineSettings.ProxySettings.proxyPort}"
+            sqService.enableProxies(PipelineSettings.ProxySettings.proxyHost, PipelineSettings.ProxySettings.proxyPort);
+        }
     }
-        
 
     // TODO - grab scan results and decorate based on branch regex (PR or not)
 
