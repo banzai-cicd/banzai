630ab295cb13a54cf5e45e889a45e548733572b7
Simon Townsend <simon.townsend1@ge.com>
2019-05-02 13:28:45 -0500

    adding call to checkmarxSastResults() after checkmarx run if config.powerDevOpsReporting exists

--- vars/checkmarx.groovy
+++ vars/checkmarx.groovy
@@ -14,28 +14,31 @@ def call(config, opts) {
 
     withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: opts.credId,
                                 usernameVariable: 'CHECKMARX_USER', passwordVariable: 'CHECKMARX_PASSWORD']]) {
-
-    step([$class: 'CxScanBuilder',
-            comment: '',
-            excludeFolders: '', 
-            excludeOpenSourceFolders: '',
-            filterPattern: '',
-            fullScanCycle: 10,
-            groupId: "${opts.teamUUID}",
-            includeOpenSourceFolders: '',
-            jobStatusOnError: 'UNSTABLE',
-            password: "${CHECKMARX_PASSWORD}",
-            preset: PRESET,
-            projectName: "${PROJECT_NAME}",
-            serverUrl: 'https://checkmarx.security.ge.com',
-            sourceEncoding: '1',
-            username: "${CHECKMARX_USER}",
-            vulnerabilityThresholdResult: 'FAILURE',
-            waitForResultsEnabled: true,
-            generatePdfReport: true
-    ])
-
+        step([$class: 'CxScanBuilder',
+                comment: '',
+                excludeFolders: '', 
+                excludeOpenSourceFolders: '',
+                filterPattern: '',
+                fullScanCycle: 10,
+                groupId: "${opts.teamUUID}",
+                includeOpenSourceFolders: '',
+                jobStatusOnError: 'UNSTABLE',
+                password: "${CHECKMARX_PASSWORD}",
+                preset: PRESET,
+                projectName: "${PROJECT_NAME}",
+                serverUrl: 'https://checkmarx.security.ge.com',
+                sourceEncoding: '1',
+                username: "${CHECKMARX_USER}",
+                vulnerabilityThresholdResult: 'FAILURE',
+                waitForResultsEnabled: true,
+                generatePdfReport: true
+        ])
     }
+
+    if (config.powerDevOpsReporting) {
+        checkmarxSastResults()
+    }
+
     if (opts.resultEmails) {
         opts.resultEmails.each {
                 logger "Emailing Checkmarx Scan Results to ${it}"
