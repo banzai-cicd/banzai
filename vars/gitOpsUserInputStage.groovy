cf796ceec4f86c100fa845110e52b73ccf0e1dcf
Simon Townsend <simon.townsend1@ge.com>
2019-05-20 13:17:35 -0500

    remove stage nesting

--- vars/gitOpsUserInputStage.groovy
+++ vars/gitOpsUserInputStage.groovy
@@ -2,7 +2,7 @@
 
 
 def call(config) {
-  def stageName = 'GitOps: User Input Stage'
+  def stageName = 'GitOps: User Input Stages'
   if (!config.gitOps || params.gitOpsTriggeringBranch != 'empty') {
       logger "Does not appear to be a user-initiated GitOps build. Skipping '${stageName}'"
       return
@@ -11,23 +11,19 @@ def call(config) {
   // prompt the user to determine which type of deployment they would like to achieve.
   // we will support 2 types first. 'version-selection' and 'environment promotion'
   
-  stage (stageName) {
-    stages {
-      stage ('Select Deployment Type') {
-        def deploymentType
-        timeout(time: 10, unit: 'MINUTES') {
-          script {
-            deploymentType = input(
-              id: 'deploymentTypeInput', 
-              message: 'What type of deployment?',
-              ok: 'Next Step',
-              parameters: [choice(name: 'deploymentType', choices: 'Version Selection\nEnvironment Promotion', description: 'What is the release scope?')]
-            )
-          }
-
-          logger "Choice selcted! ${deploymentType}"
-        }
+  stage ('Select Deployment Type') {
+    def deploymentType
+    timeout(time: 10, unit: 'MINUTES') {
+      script {
+        deploymentType = input(
+          id: 'deploymentTypeInput', 
+          message: 'What type of deployment?',
+          ok: 'Next Step',
+          parameters: [choice(name: 'deploymentType', choices: 'Version Selection\nEnvironment Promotion', description: 'What is the release scope?')]
+        )
       }
+
+      logger "Choice selcted! ${deploymentType}"
     }
   }
 }
