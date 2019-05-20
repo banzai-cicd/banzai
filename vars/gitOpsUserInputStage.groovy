84e705b897943ee6a8f0ca54eb7e4ccc839f8355
Simon Townsend <simon.townsend1@ge.com>
2019-05-20 14:25:21 -0500

    try globbing from inside the correct dir

--- vars/gitOpsUserInputStage.groovy
+++ vars/gitOpsUserInputStage.groovy
@@ -37,7 +37,10 @@ def call(config) {
   def targetEnvironment
   stage ('Target Environment?') {
     // get all of the envs listed in the repo
-    def envs = findFiles(glob: "${WORKSPACE}/envs/*").findAll { it.isDirectory() }
+    def envs
+    dir("${WORKSPACE}/envs") {
+      envs = findFiles(glob: "*/")
+    }
     if (!envs || !envs.empty) {
       logger "No environments found. Ensure that /envs is not empty"
       return
@@ -59,7 +62,10 @@ def call(config) {
 
   def targetStack
   stage ('Stack?') {
-    def stackFiles = findFiles(glob: "${WORKSPACE}/envs/${targetEnvironment}/*").findAll { !it.isDirectory() && it.getName().endsWith(".yaml") }
+    def stackFiles
+    dir("${WORKSPACE}/envs/${targetEnvironment}") {
+      stackFiles = findFiles(glob: "*.yaml")
+    }
     def stackIdChoices = stackFiles.collect { it.getName().replace('.yaml', '') }.join("\n")
     timeout(time: 10, unit: 'MINUTES') {
       script {
