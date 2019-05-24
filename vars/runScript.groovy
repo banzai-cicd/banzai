4e2cdfc11263b4dd25e16612c4c9f02d85b90eb1
Simon Townsend <simon.townsend1@ge.com>
2019-05-24 11:10:42 -0500

    typo in runScript

--- vars/runScript.groovy
+++ vars/runScript.groovy
@@ -3,27 +3,26 @@
 import org.codehaus.groovy.runtime.MethodClosure;
 
 def call(config, scriptPathOrClosure, args=null) {
-    if (scriptPathOrClosure.getClass() == MethodClosure) {
-      logger "Calling config MethodClosure"
-      scriptPathOrClosure.call(config)
-    } else if (scriptPathOrClosure.endsWith(".sh")) {
-      if (scriptPathOrClosure.charAt(0) == "/"){
-        scriptPathOrClosure = "." + scriptPathOrClosure
-      }
-      if (scriptPathOrClosure.charAt(0) != "."){
-        scriptPathOrClosure = "./" + scriptPathOrClosure
-      }
-
-      String fullPath = "${WORKSPACE}/${scriptPathOrClosure}"
-
-      if (!fileExists(fullPath)) {
-        logger "'${scriptPathOrClosure}' does not exist in the workspace!"
-        return
-      }
-
-      sh "${fullPath} ${args ? args.join(' '): ''}"
-    } else {
-      error("User-provided scripts must be .sh scripts or Groovy method closures")
+  if (scriptPathOrClosure instanceof MethodClosure) {
+    logger "Calling config MethodClosure"
+    scriptPathOrClosure.call(config)
+  } else if (scriptPathOrClosure.endsWith(".sh")) {
+    if (scriptPathOrClosure.charAt(0) == "/"){
+      scriptPathOrClosure = "." + scriptPathOrClosure
     }
+    if (scriptPathOrClosure.charAt(0) != "."){
+      scriptPathOrClosure = "./" + scriptPathOrClosure
+    }
+
+    String fullPath = "${WORKSPACE}/${scriptPathOrClosure}"
+
+    if (!fileExists(fullPath)) {
+      logger "'${scriptPathOrClosure}' does not exist in the workspace!"
+      return
+    }
+
+    sh "${fullPath} ${args ? args.join(' '): ''}"
+  } else {
+    error("User-provided scripts must be .sh scripts or Groovy method closures")
   }
 }
