5be5c5066704454871c55c3d4c78093b569d9b7a
Simon Townsend <simon.townsend1@ge.com>
2019-05-24 10:59:23 -0500

    logging

--- vars/getBranchBasedStageConfig.groovy
+++ vars/getBranchBasedStageConfig.groovy
@@ -4,9 +4,12 @@
  returns false OR an object representing the config for the stage
 */
 def call(stageConfig) {
+    logger "testing stageConfig ${stageConfig}"
     if (stageConfig instanceof String) {
+        logger "String-based branch config"
         return BRANCH_NAME ==~ stageConfig ? [:] : false
     } else if (stageConfig instanceof Map) {
+        logger "Map-based branch config"
         def key = stageConfig.keySet().find { BRANCH_NAME ==~ it }
         return key ? stageConfig[key] : false
     } else {
