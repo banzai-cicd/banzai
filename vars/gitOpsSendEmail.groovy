e6f4486cd6e412e4847209a989780d0e946cdb53
Simon Townsend <simon.townsend1@ge.com>
2019-05-24 09:26:34 -0500

    GitOps: update url in email to Blue Ocean

--- vars/gitOpsSendEmail.groovy
+++ vars/gitOpsSendEmail.groovy
@@ -2,7 +2,8 @@
 
 def call(to, cc, subject, body) {
   logger "Sending Email to ${to}: ${subject}"
-  String jobInfo = "Job: ${env.JOB_NAME} #${env.BUILD_NUMBER} \nBuild URL: ${env.BUILD_URL}\n\n"
+  String url = env.RUN_DISPLAY_URL ?: env.BUILD_URL
+  String jobInfo = "Job: ${env.JOB_NAME} #${env.BUILD_NUMBER} \nBuild URL: ${url}\n\n"
   mail from: "JenkinsAdmin@ge.com",
     to: to,
     cc: cc,
