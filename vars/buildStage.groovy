ee72d594897b147ec0167b359261bb9ad4b00552
Simon Townsend <simon.townsend1@ge.com>
2019-05-24 11:01:29 -0500

    logging

--- vars/buildStage.groovy
+++ vars/buildStage.groovy
@@ -3,6 +3,7 @@
 def call(config) {
   def stageName = 'Build'
   def stageConfig = getBranchBasedStageConfig(config.build)
+  logger "Build stageConfig ${stageConfig}"
   if (!stageConfig) {
     logger "${BRANCH_NAME} does not match a 'build' branch pattern. Skipping ${stageName}"
     return
