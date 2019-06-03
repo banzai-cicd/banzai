071ed9e32f4670a200e60f078c17ca1884aed9a4
Simon Townsend <simon.townsend1@ge.com>
2019-06-03 15:16:05 -0500

    fix gitops deploy conditional

--- vars/deployStage.groovy
+++ vars/deployStage.groovy
@@ -2,14 +2,15 @@
 
 def call(config) {
   def stageName = 'Deploy'
-  def stageConfig
   
   if (config.gitOps) {
     if (!config.internal.gitOps.DEPLOY) {
       // if this is a GitOps repo then config.internal.gitOps.DEPLOY must be set
-      logger "${BRANCH_NAME} does qualify for deployment. Skipping ${stageName}"
+      logger "${BRANCH_NAME} does qualify for GitOps deployment. Skipping ${stageName}"
       return
     }
+
+    stageConfig = [:]
   } else {
     // see if this is a project repo with a deployment configuration
     stageConfig = getBranchBasedConfig(config.deploy)
