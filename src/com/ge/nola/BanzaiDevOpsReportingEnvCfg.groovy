adb65fdc79d87a9aad2695c29328771404b07e9b
Simon Townsend <simon.townsend1@ge.com>
2019-06-03 10:29:26 -0500

    add asMap method to BanzaiDevOpsReportingEnvCfg

--- src/com/ge/nola/BanzaiDevOpsReportingEnvCfg.groovy
+++ src/com/ge/nola/BanzaiDevOpsReportingEnvCfg.groovy
@@ -3,4 +3,10 @@ package com.ge.nola;
 class BanzaiDevOpsReportingEnvCfg {
     int key
     String name
+
+    Map asMap() {
+        this.class.declaredFields.findAll { !it.synthetic }.collectEntries {
+            [ (it.name):this."$it.name" ]
+        }
+    }
 }
