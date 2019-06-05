145e5ccd4c864f161b2e087f4d872a2686bad97a
Simon Townsend <simon.townsend1@ge.com>
2019-06-05 14:25:01 -0500

    remove logging

--- vars/notifyEmail.groovy
+++ vars/notifyEmail.groovy
@@ -18,33 +18,26 @@ void call(BanzaiCfg cfg, BanzaiEvent event) {
     }
 
     String currentEvent = event.getEventLabel()
-    logger "currentEvent: ${currentEvent}"
     Set<String> addresses = []
     if (emailCfg.groups) {
         // find groupIds
         Set<String> groupIds = emailCfg.groups.keySet().findAll { groupId ->
              emailCfg.groups[groupId].find { regex -> currentEvent ==~ regex }
         }
-        if (groupIds.size() == 0) { return }
-
-        logger "groupIds: ${groupIds}"
         
         groupIds.each { groupId ->
             List<String> emailIds = cfg.email.groups[groupId]
             emailIds.each { addresses.add(cfg.email.addresses[it]) }
         }
     }
-    logger "addresses1: ${addresses}"
     if (emailCfg.individuals) {
         Set<String> emailIds = emailCfg.individuals.keySet().findAll { emailId ->
             emailCfg.individuals[emailId].find { regex -> currentEvent ==~ regex }
         }
-        if (emailIds.size() == 0) { return }
 
-        logger "emailIds: ${emailIds}"
         emailIds.each { addresses.add(cfg.email.addresses[it]) }
     }
-    logger "addresses2: ${addresses}"
+
     if (addresses.size() > 0) {
         String details = "${env.JOB_NAME} ${event.scope} ${event.status}"
         String subject = "Banzai: ${details}"
