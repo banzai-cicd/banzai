a754326ac28302ca1580bae48e1b79dabcefdc16
Simon Townsend <simon.townsend1@ge.com>
2019-06-10 10:03:09 -0500

    bugs n bugs

--- vars/sendEmail.groovy
+++ vars/sendEmail.groovy
@@ -4,9 +4,9 @@ def call(to, subject, body, attachmentsPattern) {
   logger "Sending Email to ${to}: ${subject}"
   String url = env.RUN_DISPLAY_URL ?: env.BUILD_URL
   String jobInfo = "Job: ${env.JOB_NAME} #${env.BUILD_NUMBER} \nBuild URL: ${url}\n\n"
-  emailext from: 'JenkinsAdmin@ge.com'
+  emailext from: 'JenkinsAdmin@ge.com',
     to: to,
     subject:"Banzai: ${subject}",
-    body: "${jobInfo}${body}"
+    body: "${jobInfo}${body}",
     attachmentsPattern: attachmentPattern
 }
