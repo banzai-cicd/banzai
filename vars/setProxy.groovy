e9d4af8e3b0b7e5dc55e7b71c6dbb36700682e78
Simon Townsend <simon.townsend1@ge.com>
2019-06-26 14:26:35 -0500

    ensure proxy protocol is set

--- vars/setProxy.groovy
+++ vars/setProxy.groovy
@@ -1,8 +1,7 @@
 #!/usr/bin/env groovy
-
+// TODO: hate this..refactor
 def call(config) {
-    // set proxy info from Environment if applicable
-    if (config.httpsProxy && config.httpsProxy.envVar) {
+    if (config.httpsProxy?.envVar) {
         logger "Setting HTTPS Proxy from environment variable ${config.httpsProxy.envVar}"
         def hostAndPort = env[config.httpsProxy.envVar].tokenize(":")
         config.httpsProxy = [
@@ -12,7 +11,7 @@ def call(config) {
         ]
         config.noProxy = config.noProxy ?: env.no_proxy
     }
-    if (config.httpProxy && config.httpProxy.envVar) {
+    if (config.httpProxy?.envVar) {
         logger "Setting HTTP Proxy from environment variable ${config.httpProxy.envVar}"
         def hostAndPort = env[config.httpProxy.envVar].tokenize(":")
         config.httpProxy = [
@@ -25,10 +24,12 @@ def call(config) {
     }
 
     if (config.httpsProxy) {
+        config.httpsProxy.protocol = 'https'
         logger "HTTPS PROXY set to ${config.httpsProxy.getUrl()}"
     }
 
     if (config.httpProxy) {
+        config.httpProxy.protocol = 'http'
         logger "HTTP PROXY set to ${config.httpProxy.getUrl()}"
     }
 
