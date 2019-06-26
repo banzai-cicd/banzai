f5e8c6a6d7b1d98ef896d4b8aed8b3f8c726419a
Simon Townsend <simon.townsend1@ge.com>
2019-06-26 09:18:16 -0500

    sonar

--- vars/sonarQube.groovy
+++ vars/sonarQube.groovy
@@ -2,8 +2,8 @@
 import com.ge.nola.cfg.BanzaiQualityCfg
 
 def call(BanzaiQualityCfg cfg) {
-    withSonarQubeEnv('SonarQube') {
-        String toolName = cfg.tools?.sonarQube ?: 'SonarQube'
+    String toolName = cfg.tools?.sonarQube ?: 'SonarQube'
+    withSonarQubeEnv(toolName) {
         def sonarqubeScannerHome = tool name: toolName, type: 'hudson.plugins.sonar.SonarRunnerInstallation';
         sh "${sonarqubeScannerHome}/bin/sonar-scanner -e -X -Dsonar.projectKey=${cfg.projectKey} -Dsonar.sources=.";
     }
